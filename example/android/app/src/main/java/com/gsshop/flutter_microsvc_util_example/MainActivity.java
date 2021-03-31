5ce72431ce5b2778eed0de3fb211afdf0364bcb0
TaeHeun Lee <nixstory@gmail.com>
2021-04-01 03:05:59 +0900

    flutter_microsvc_util

---
+++ example/android/app/src/main/java/com/gsshop/flutter_microsvc_util_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.gsshop.flutter_microsvc_util_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Classes/FlutterMicrosvcUtilPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlutterMicrosvcUtilPlugin : NSObject<FlutterPlugin>
+@end
